667e20f0e34f71a8bdf54accccf253d44da588e0
Vipul <007vipulrank1@gmail.com>
2023-10-29 13:45:00 +0530

    ADD: client validation and error handling

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,18 +2,20 @@ import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 import prisma from '@/prisma/client'
 
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1)
+export const createIssueSchema = z.object({
+  title: z.string().min(1, { message: 'Title is Required' }).max(255),
+  description: z.string().min(1, { message: 'Description is Required' })
 })
 
 export async function POST(req: NextRequest) {
   const body = await req.json()
 
+  // return NextResponse.json({message: "Something went wrong"}, {status: 400})
+
   const validation = createIssueSchema.safeParse(body)
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400, statusText: 'NOT OK' })
+    return NextResponse.json(validation.error.format(), { status: 400, statusText: 'NOT OK' })
   }
 
   const newIssue = await prisma.issue.create({
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import axios from 'axios'
-import React from 'react'
+import axios, { AxiosError } from 'axios'
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import { Controller, useForm } from 'react-hook-form'
-import SimpleMDE from 'react-simplemde-editor'
+import { CiCircleInfo } from 'react-icons/ci'
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
+
 import 'easymde/dist/easymde.min.css'
+
+import { createIssueSchema } from '@/app/api/issues/route'
 
 interface IssueForm {
   title: string
@@ -16,26 +23,72 @@ interface IssueForm {
 type NewIssuePageProps = {}
 
 const NewIssuePage: React.FC<NewIssuePageProps> = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const [err, setErr] = useState<string | undefined>(undefined)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset
+  } = useForm<IssueForm>({
+    defaultValues: {
+      title: '',
+      description: ''
+    },
+    resolver: zodResolver(createIssueSchema)
+  })
   const router = useRouter()
 
   const onSubmit = async (data: IssueForm) => {
-    const res = await axios.post('/api/issues', { ...data })
-
-    router.push('/issues')
+    try {
+      const res = await axios.post('/api/issues', { ...data })
+      reset()
+      router.push('/issues')
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setErr(error.response?.data.message ?? error.message)
+      } else if (error instanceof Error) {
+        setErr(error.message)
+      } else {
+        setErr('An Unexpected Error Has Occured!')
+      }
+    } finally {
+      setTimeout(() => setErr(undefined), 3500)
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit((data) => onSubmit(data))} className="max-w-screen-sm space-y-4">
-      <TextField.Root size={'3'}>
-        <TextField.Input placeholder="Title" {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
-      />
-      <Button size={'3'}>Submit New Issue</Button>
+    <form onSubmit={handleSubmit((data) => onSubmit(data))} className="max-w-screen-sm space-y-4 mx-auto">
+      {err && (
+        <Callout.Root color="red">
+          <Callout.Icon>
+            <CiCircleInfo size={20} />
+          </Callout.Icon>
+          <Callout.Text>{err}</Callout.Text>
+        </Callout.Root>
+      )}
+      <div>
+        <TextField.Root size={'3'}>
+          <TextField.Input
+            placeholder="Title"
+            {...(errors.title?.message && { variant: 'soft', color: 'red' })}
+            {...register('title')}
+          />
+        </TextField.Root>
+        <p className="mt-2 text-sm text-red-600 font-medium">{errors.title?.message}</p>
+      </div>
+
+      <div>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Description" {...field} ref={null} />}
+        />
+        <p className="mt-2 text-sm text-red-600 font-medium">{errors.description?.message}</p>
+      </div>
+      <Button size={'3'} disabled={isSubmitting}>
+        Submit New Issue
+      </Button>
     </form>
   )
 }
