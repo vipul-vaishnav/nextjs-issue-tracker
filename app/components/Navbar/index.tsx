46682ce9331015301edc95ba9ce40124226128f0
Vipul <007vipulrank1@gmail.com>
2023-10-29 11:07:40 +0530

    Style active link and add issues page

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,10 +1,15 @@
-import Link from 'next/link'
+'use client'
+
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 
 type NavbarProps = {}
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const path = usePathname()
+
   return (
     <nav className="flex items-center justify-between border-b border-zinc-700 px-6 h-16 max-w-screen-2xl mx-auto">
       <Link href="/">
@@ -15,12 +20,20 @@ const Navbar: React.FC<NavbarProps> = () => {
 
       <ul className="flex items-center gap-6">
         <li>
-          <Link href="/" className="transition-all duration-300 hover:opacity-50">
+          <Link
+            href="/"
+            className={`transition-all duration-300 hover:opacity-50 ${path === '/' ? 'opacity-50 underline' : ''}`}
+          >
             Dashboard
           </Link>
         </li>
         <li>
-          <Link href="/issues" className="transition-all duration-300 hover:opacity-50">
+          <Link
+            href="/issues"
+            className={`transition-all duration-300 hover:opacity-50 ${
+              path === '/issues' ? 'opacity-50 underline' : ''
+            }`}
+          >
             Issues
           </Link>
         </li>
---
+++ app/issues/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+type IssuesPageProps = {}
+
+const IssuesPage: React.FC<IssuesPageProps> = () => {
+  return <div>Have a good coding</div>
+}
+export default IssuesPage
